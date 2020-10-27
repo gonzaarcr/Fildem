gonzaarcr/Fildem

commit 9c6754daab58a1536e1e76fccd308d697f16c942

HUD uses normalized actions

utils/menu.py
@@ -113,7 +113,7 @@ class DbusGtkMenu(object):
 				if ':submenu' in menu:
 					self.collect_entries(menu[':submenu'], menu_path)
 				elif 'action' in menu:
-					self.actions[menu_item.text] = menu_item.action
+					self.actions[menu_item.text.replace('_', '')] = menu_item.action
 					self.items.append(menu_item)
 
 			elif ':section' in menu:
@@ -249,7 +249,7 @@ class DbusAppMenu(object):
 				self.collect_entries(child, menu_path)
 
 		elif bool(menu_item.label) or menu_item.separator:
-			self.actions[menu_item.text] = menu_item.action
+			self.actions[menu_item.text.replace('_', '')] = menu_item.action
 			self.items.append(menu_item)
 
 class DbusMenu:

handlers/default.py
@@ -196,7 +196,6 @@ class CommandList(Gtk.ListBox):
 		return item.visibility()
 
 	def do_list_item(self, value, index):
-		value = value.replace('_', '')
 		command = CommandListItem(value=value, index=index)
 
 		self.append_visible_row(command)

commit dc9e3920592dbb23a931e89c700dade6ef5e6ed3

Fixes bug introduced by 9c6754d

utils/menu.py
@@ -113,7 +113,7 @@ class DbusGtkMenu(object):
 				if ':submenu' in menu:
 					self.collect_entries(menu[':submenu'], menu_path)
 				elif 'action' in menu:
-					self.actions[menu_item.text.replace('_', '')] = menu_item.action
+					self.actions[menu_item.text] = menu_item.action
 					self.items.append(menu_item)
 
 			elif ':section' in menu:
@@ -249,7 +249,7 @@ class DbusAppMenu(object):
 				self.collect_entries(child, menu_path)
 
 		elif bool(menu_item.label) or menu_item.separator:
-			self.actions[menu_item.text.replace('_', '')] = menu_item.action
+			self.actions[menu_item.text] = menu_item.action
 			self.items.append(menu_item)
 
 class DbusMenu: