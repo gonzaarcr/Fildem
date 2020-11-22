36d489dd2e458c171df3fa6fc8ebecfe757e3c2e
Gonzalo <gonza.ar.cr@gmail.com>
2020-11-22 02:49:46 -0300

    Functions removed by mistake

--- command.py
+++ command.py
@@ -15,6 +15,18 @@ def run_hud_menu(menu):
 	run_command('appmenu', 'main')
 	run_command('keybinder', menu)
 
+def global_hud_menu(accel, dbus_menu):
+	menu = GlobalMenu(dbus_menu)
+	menu.run()
+
+def default_hud_menu(accel, dbus_menu):
+	menu = HudMenu(dbus_menu)
+	menu.run()
+
+def rofi_hud_menu(*args):
+	menu = RofiMenu()
+	menu.run()
+
 def main():
 	run_hud_menu('main')
 
