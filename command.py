a9dab23910f4ab94a786f3bc6c065c58e71b98fa
Gonzalo <gonza.ar.cr@gmail.com>
2020-11-22 02:34:23 -0300

    Removed the tty check

--- command.py
+++ command.py
@@ -1,12 +1,7 @@
 #! /usr/bin/python3
 
 import os
-import sys
 import threading
-
-from handlers.default import HudMenu
-from handlers.global_menu import GlobalMenu
-from handlers.rofi import RofiMenu
 
 
 def run_command(module, function):
@@ -16,48 +11,19 @@ def run_command(module, function):
 	proc = threading.Thread(target=os.system, args=[args])
 	proc.start()
 
-
 def run_hud_menu(menu):
 	run_command('appmenu', 'main')
 	run_command('keybinder', menu)
 
-
-def global_hud_menu(accel, dbus_menu):
-	menu = GlobalMenu(dbus_menu)
-	menu.run()
-
-
-def default_hud_menu(accel, dbus_menu):
-	menu = HudMenu(dbus_menu)
-	menu.run()
-
-
-def rofi_hud_menu(*args):
-	menu = RofiMenu()
-	menu.run()
-
-
 def main():
-	if sys.stdin.isatty():
-		run_hud_menu('main')
-	else:
-		default_hud_menu()
-
+	run_hud_menu('main')
 
 def global_menu():
-	if sys.stdin.isatty():
-		run_hud_menu('global_menu')
-	else:
-		global_hud_menu()
-
+	run_hud_menu('global_menu')
 
 def rofi():
-	if sys.stdin.isatty():
-		run_hud_menu('rofi')
-	else:
-		rofi_hud_menu()
+	run_hud_menu('rofi')
 
 
 if __name__ == "__main__":
 	main()
-	# global_menu()
