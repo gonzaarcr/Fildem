a877a9c2f356a8931bcbc1d0f83c8d0a2d4f6271
Gonzalo <gonza.ar.cr@gmail.com>
2021-02-18 04:09:21 -0300

    Added run.py

---
+++ run.py
@@ -0,0 +1,19 @@
+#!/usr/bin/pytohn3
+
+import os
+import sys
+
+from command import main as command_main
+
+def main():
+	if sys.path[0] != '':
+		os.chdir(sys.path[0])
+
+	if os.environ['XDG_SESSION_TYPE'] == 'x11':
+		os.environ['GDK_BACKEND'] = 'x11'
+
+	os.environ['UBUNTU_MENUPROXY'] = '0'
+	command_main()
+	
+if __name__ == '__main__':
+	main()
