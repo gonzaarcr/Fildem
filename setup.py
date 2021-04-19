0a9f58970a8dbe90675faf43857b328c830e021c
Gonzalo <gonza.ar.cr@gmail.com>
2021-04-19 02:39:56 -0300

    Changed anatomy

---
+++ setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name='fildem',
+	version='0.6.1',
+	author='Gonzalo',
+	author_email='gonzaarcr@gmail.com',
+	description='Fildem Global Menu for Gnome Desktop',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://github.com/gonzaarcr/Fildem',
+	packages=setuptools.find_packages(),
+	data_files=[
+		('share/applications', ['fildem-hud.desktop'])
+	],
+	install_requires=[
+		'PyGObject>=3.30.0',
+		'fuzzysearch>=0.5.0'
+	],
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		'Operating System :: POSIX :: Linux'
+	],
+	project_urls={
+		'Bug Reports': 'https://github.com/gonzaarcr/Fildem/issues',
+		'Source': 'https://github.com/gonzaarcr/Fildem',
+	},
+	entry_points={
+		'console_scripts': [
+			'fildem = fildem.run:main',
+			'fildem-hud = fildem.inithud:main'
+		]
+	}
+)
