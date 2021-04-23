f12634005953a47d5f1ed9d311496ec3093977c0
Gonzalo <gonza.ar.cr@gmail.com>
2021-04-23 00:30:37 -0300

    GDK_BACKEND=x11 to Wayland

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ def main():
 	if sys.path[0] != '':
 		os.chdir(sys.path[0])
 
-	if os.environ['XDG_SESSION_TYPE'] == 'x11':
+	if os.environ['XDG_SESSION_TYPE'] != 'x11':
 		os.environ['GDK_BACKEND'] = 'x11'
 
 	os.environ['UBUNTU_MENUPROXY'] = '0'
